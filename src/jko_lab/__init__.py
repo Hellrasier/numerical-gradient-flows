9b8afa1d1494c810fa80262f49c7a6fe5a453f18
Dohyoung Ko <dzk5572@gmail.com>
2025-11-06 17:41:57 -0500

    resolve import issues

--- src/jko_lab/__init__.py
+++ src/jko_lab/__init__.py
@@ -1,12 +1,10 @@
-from .pdhg_jko import PrimalDualJKO, pdhg_jko, jko_flow, proxF_entropy, proxF_quadratic
+from .pdhg_jko import PrimalDualJKO, proxF_entropy, proxF_quadratic
 from .sinkhorn_jko import Sinkhorn, SinkhornJKO, col_sums, row_sums
 
 __all__ = [
     "PrimalDualJKO",
     "proxF_entropy",
     "proxF_quadratic",
-    "pdhg_jko",
-    "jko_flow",
     "Sinkhorn",
     "SinkhornJKO",
     "col_sums",
